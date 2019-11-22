593f3527c79129d4c186341adcb5bbdaea10b3de
DESKTOP-SU1PR5F\hello2 <xodud5621@gmail.com>
2019-11-22 16:37:48 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
 
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/any/1',
+    'name': '정태영',
+    'birthday': '991114',
+    'gender': '남자',
+    'job': '프로그래머'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/any/2',
+    'name': '홍길동',
+    'birthday': '950524',
+    'gender': '남자',
+    'job': '프로그래머'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/any/3',
+    'name': '나동빈',
+    'birthday': '990511',
+    'gender': '남자',
+    'job': '프로그래머'
+  }
+]
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} alt="logo" />
-      <h2>Let's develop</h2>
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          )
+        })
+      }
+
     </div>
   );
 }
