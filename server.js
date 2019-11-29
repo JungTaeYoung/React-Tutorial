4e3f69b4c39ab277be4bb47e72539f24147d1d3e
unknown <xodud5621@naver.com>
2019-11-29 15:25:03 +0900

    Add Get Customers REST API Module

--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-  res.send({ message: 'Hello Express!' })
+app.get('/api/customers', (req, res) => {
+  res.send([
+    {
+      'id': 1,
+      'image': 'https://placeimg.com/64/64/any/1',
+      'name': '정태영',
+      'birthday': '991114',
+      'gender': '남자',
+      'job': '프로그래머'
+    },
+    {
+      'id': 2,
+      'image': 'https://placeimg.com/64/64/any/2',
+      'name': '홍길동',
+      'birthday': '950524',
+      'gender': '남자',
+      'job': '프로그래머'
+    },
+    {
+      'id': 3,
+      'image': 'https://placeimg.com/64/64/any/3',
+      'name': '나동빈',
+      'birthday': '990511',
+      'gender': '남자',
+      'job': '프로그래머'
+    }
+  ])
 })
 
-app.listen(port, ()=> console.log(`listening on port ${port}`));
+app.listen(port, () => console.log(`listening on port ${port}`));
